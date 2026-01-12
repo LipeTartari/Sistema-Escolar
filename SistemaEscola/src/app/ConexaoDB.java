129b9b26d5fa32afbca25fd3d37c0147620ad95d
LipeTartari <luizfelipetartari@gmail.com>
2026-01-12 10:41:54 -0300

    correção de erro

--- SistemaEscola/src/app/ConexaoDB.java
+++ SistemaEscola/src/app/ConexaoDB.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 class ConexaoDB {
     private static final String URL = "jdbc:postgresql://localhost:5432/escola_at";
     private static final String USER = "postgres";
-    private static final String PASSWORD = "TARTARI1212";
+    private static final String PASSWORD = "SUA_SENHA_AQUI";
 
     public static Connection conectar() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
